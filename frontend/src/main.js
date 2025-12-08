18f347643861adea6ad5959001edf7c0970aa214
Zamuro72 <zafde91@gmail.com>
2025-12-08 14:09:32 +0700

    Fix store import path

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,10 +2,4 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import './style.css'
 
-const app = createApp(App)
-app.mount('#app')
-import { createApp } from 'vue'
-import App from './App.vue'
-import './style.css'
-
 createApp(App).mount('#app')
